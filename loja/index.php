c3e95617c73967c92bcb5e2c562447eb242cd761
discente <discente@uirapuru.br>
2026-09-25 17:52:59 -0300

    update: estilização da tela e cadastrar produtos

--- loja/index.php
+++ loja/index.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Formulario de Cadastro</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+
 <body>
     <section>
-        <a href="produtos.php" class="sombra">Ver todos os produtos</a>
-        <form method="post" enctype="multipart/form-data">
+
+        <div class="form-pai">
+            <form method="post" enctype="multipart/form-data">
             <h1>ENVIO DE IMAGENS</h1>
             <label for="nome">Nome do Produto</label>
             <input type="text" name="nome" id="nome" class="sombra">
@@ -19,76 +22,84 @@
             <input type="number" name="valor" id="val" class="sombra" step="0.01" min="0">
 
             <input type="file" name="foto[]" multiple id="foto" class="sombra meuInput">
-            <input type="submit" id="botao" value="Enviar">
+
+            <div class="test">
+                <input type="submit" id="botao" value="Enviar">
+                <a href="produtos.php" class="sombra">Ver todos os produtos</a>
+            </div>
+
         </form>
+        </div>
+
     </section>
 
-<?php
-//checa se o usuario clicou no botao (preencheu ao menos o nome)
-if(isset($_POST['nome']) && !empty($_POST['nome'])){
+    <?php
+    //checa se o usuario clicou no botao (preencheu ao menos o nome)
+    if (isset($_POST['nome']) && !empty($_POST['nome'])) {
 
-    //coloca o dado preenchido em uma variavel e checa se nao tem injection
-    $nome      = addslashes($_POST['nome']);
-    $descricao = addslashes($_POST['desc']);
-    $valor     = addslashes($_POST['valor']);
+        //coloca o dado preenchido em uma variavel e checa se nao tem injection
+        $nome = addslashes($_POST['nome']);
+        $descricao = addslashes($_POST['desc']);
+        $valor = addslashes($_POST['valor']);
 
-    //cria um array vazio para guardar os nomes das fotos caso tenha enviado
-    $fotos = array();
+        //cria um array vazio para guardar os nomes das fotos caso tenha enviado
+        $fotos = array();
 
-    //checa se o usuario selecionou alguma imagem
-    if(isset($_FILES['foto'])){
-        $tipo = '';
+        //checa se o usuario selecionou alguma imagem
+        if (isset($_FILES['foto'])) {
+            $tipo = '';
 
-        //cria um laco e repete enquanto houver fotos
-        for($i = 0; $i < count($_FILES['foto']['name']); $i++){
+            //cria um laco e repete enquanto houver fotos
+            for ($i = 0; $i < count($_FILES['foto']['name']); $i++) {
 
-            //checa a extensao para poder enviar apenas PNG e JPG
-            if($_FILES['foto']['type'][$i] == "image/png"){
-                $tipo = ".png";
-            }elseif($_FILES['foto']['type'][$i] == 'image/jpeg'){
-                $tipo = ".jpg";
-            }else{
-                $tipo = "outro";
-            }
+                //checa a extensao para poder enviar apenas PNG e JPG
+                if ($_FILES['foto']['type'][$i] == "image/png") {
+                    $tipo = ".png";
+                } elseif ($_FILES['foto']['type'][$i] == 'image/jpeg') {
+                    $tipo = ".jpg";
+                } else {
+                    $tipo = "outro";
+                }
 
-            if($tipo == 'outro'){
-                ?>
-                <script>
-                    alert("Só é possível enviar arquivos JPG e PNG");
-                </script>
-                <?php
-            }else{
-                //gera um nome unico (evita sobrepor arquivo com mesmo nome)
-                $nome_arquivo = md5($_FILES['foto']['name'][$i]).rand(1,999).$tipo;
+                if ($tipo == 'outro') {
+                    ?>
+                    <script>
+                        alert("Só é possível enviar arquivos JPG e PNG");
+                    </script>
+                    <?php
+                } else {
+                    //gera um nome unico (evita sobrepor arquivo com mesmo nome)
+                    $nome_arquivo = md5($_FILES['foto']['name'][$i]) . rand(1, 999) . $tipo;
 
-                //move o arquivo da pasta temporaria para a pasta imagens, ja com o nome novo
-                move_uploaded_file($_FILES['foto']['tmp_name'][$i], 'imagens/'.$nome_arquivo);
+                    //move o arquivo da pasta temporaria para a pasta imagens, ja com o nome novo
+                    move_uploaded_file($_FILES['foto']['tmp_name'][$i], 'imagens/' . $nome_arquivo);
 
-                //armazena o nome do arquivo no vetor fotos
-                array_push($fotos, $nome_arquivo);
+                    //armazena o nome do arquivo no vetor fotos
+                    array_push($fotos, $nome_arquivo);
+                }
             }
         }
-    }
 
-    //verifica se todos os campos obrigatorios foram preenchidos
-    if(!empty($nome) && !empty($descricao) && !empty($valor)){
-        require 'classes/Produto.class.php';
-        $p = new Produto();
-        $isOk = $p->enviarProduto($nome, $descricao, $valor, $fotos);
+        //verifica se todos os campos obrigatorios foram preenchidos
+        if (!empty($nome) && !empty($descricao) && !empty($valor)) {
+            require 'classes/Produto.class.php';
+            $p = new Produto();
+            $isOk = $p->enviarProduto($nome, $descricao, $valor, $fotos);
 
-        if($isOk){
-            echo "<script>alert('Produto cadastrado com sucesso!');</script>";
-        }else{
-            echo "<script>alert('Erro ao cadastrar o produto.');</script>";
+            if ($isOk) {
+                echo "<script>alert('Produto cadastrado com sucesso!');</script>";
+            } else {
+                echo "<script>alert('Erro ao cadastrar o produto.');</script>";
+            }
+        } else {
+            ?>
+            <script>
+                alert("Preencha os campos obrigatorios!")
+            </script>
+            <?php
         }
-    }else{
-        ?>
-        <script>
-            alert("Preencha os campos obrigatorios!")
-        </script>
-        <?php
     }
-}
-?>
+    ?>
 </body>
+
 </html>
